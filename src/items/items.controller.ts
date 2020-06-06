9a8016a9ab456e7708d8e2cc48131178ad4e496b
Tyler Boyd <tylerb@theboydprojects.com>
2020-06-06 13:30:54 -0500

    Added the ability to protect routes and roles

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards, } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { Items } from '../items';
 import { Item } from '../item';
+import { AuthGuard } from '@nestjs/passport';
+import { Permissions } from '../permissions.decorator';
+import { PermissionsGuard } from '../permissions.guard';
 
 @Controller('items')
 export class ItemsController {
@@ -17,17 +20,23 @@ export class ItemsController {
         return this.itemsService.find(id);
     }
 
+    @UseGuards(AuthGuard('jwt'), PermissionsGuard)
     @Post()
+    @Permissions('create:items')
     create(@Body('item') item: Item) {
         this.itemsService.create(item);
     }
 
+    @UseGuards(AuthGuard('jwt'), PermissionsGuard)
     @Put()
+    @Permissions('update:items')
     update(@Body('item') item: Item) {
         this.itemsService.update(item);
     }
 
+    @UseGuards(AuthGuard('jwt'), PermissionsGuard)
     @Delete('id')
+    @Permissions('delete:items')
     delete(@Param('id') id: number) {
         this.itemsService.delete(id);
     }
